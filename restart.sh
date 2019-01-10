e0a68ead6b3c2359be4501ab4970bb36b40f87ad
johnny <johnny@stargazer.com.sg>
2019-01-10 16:56:04 +0800

    setting

--- restart.sh
+++ restart.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 ps -ef | grep java | grep mdb |  awk '{print $2}' | xargs kill
 cd /tmp/mdb
 git reset --hard 92ecc1102295625344b727a0f4b8178e46fcc0e5
 git pull 
 mvn clean compile assembly:single 
 sleep 5
-java -jar /tmp/mdb/target/*.jar > app.log 2>&1 & 
+#java -jar /tmp/mdb/target/*.jar > app.log 2>&1 & 
--- start-load-generator.sh
+++ start-load-generator.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 OPT="-Xms256m -Xms256m"
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 0 > /tmp/client-0.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 1 > /tmp/client-1.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 2 > /tmp/client-2.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 3 > /tmp/client-3.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 4 > /tmp/client-4.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 5 > /tmp/client-5.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 6 > /tmp/client-6.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 7 > /tmp/client-7.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 8 > /tmp/client-8.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 9 > /tmp/client-9.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 10 > /tmp/client-10.log 2>&1 &
-java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 11 > /tmp/client-11.log 2>&1 &
+i=0
+end=$1
+while [ $i -le $end ]; do
+	echo "java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 $i > /tmp/client-$i.log 2>&1 &"
+	java $OPT -jar /tmp/mdb/target/*.jar  10.42.12.205 $i > /tmp/client-$i.log 2>&1 &
+    i=$(($i+1))
+done
